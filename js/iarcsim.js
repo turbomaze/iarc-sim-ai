3f0c7876d2078e77ad0805e1b01aa66a1f247f19
Anthony Liu <anthonyscottliu@gmail.com>
2015-10-24 20:23:30 -0400

    Moves roombas radiall outwards.

--- js/iarcsim.js
+++ js/iarcsim.js
@@ -18,11 +18,13 @@ var IARCSim = (function() {
      * working vars */
     var canvas, ctx;
     var roombas;
+    var lastRenderTime;
 
     /*************
      * constants */
     var CENTER = [DIMS[0]/2, DIMS[1]/2];
-    var R = 7;
+    var R = 7; //radius of the Roombas
+    var S = 20; //px per second
     var INIT_D = 50;
 
     /***********
@@ -30,6 +32,12 @@ var IARCSim = (function() {
     function Roomba(pos, color) {
       this.position = pos.slice(0);
       this.color = color;
+      this.direc = norm([
+        this.position[0] - CENTER[0],
+        this.position[1] - CENTER[1]
+      ]);
+      this.r = R; //radius
+      this.s = S; //speed
     }
 
     /******************
@@ -50,19 +58,50 @@ var IARCSim = (function() {
         );
       }
 
+      lastRenderTime = +new Date();
       render();
     }
 
     function render() {
       clearCanvas();
 
+      var t = +new Date();
+      var dt = t - lastRenderTime;
+      lastRenderTime = t;
+
       roombas.map(drawRoomba);
+      roombas.map(function(roomba) {
+        updateRoomba(roomba, dt);
+      });
+
+      requestAnimationFrame(render);
     }
 
     /********************
      * helper functions */
+    function norm(a) {
+      var mag = Math.sqrt(a.reduce(function(acc, curr) {
+        return acc + curr*curr;
+      }, 0));
+      return a.map(function(comp) {
+        return comp/mag;
+      });
+    }
+
+    function updateRoomba(roomba, time) {
+      var ds = roomba.s * time/1000;
+      roomba.position[0] += ds * roomba.direc[0];
+      roomba.position[1] += ds * roomba.direc[1];
+    }
+
     function drawRoomba(roomba) {
+      //represent the Roomba
       Crush.drawPoint(ctx, roomba.position, R, roomba.color);
+      //velocity
+      Crush.drawLine(ctx, roomba.position, [
+        roomba.position[0]+2*roomba.r*roomba.direc[0],
+        roomba.position[1]+2*roomba.r*roomba.direc[1]
+      ], 'red', 2);
     }
 
     function clearCanvas() {
